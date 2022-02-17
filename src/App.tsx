6df9079e37c2bac755f9de67f91bcaa44bea28af
GabrielRioo <gabrielpaivario@gmail.com>
2022-02-17 00:29:29 -0300

    Criando um formulario d registro

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css';
 // import FormikContainer from './components/FormikContainer'
 // import NewYoutubeForm from './components/NewYoutubeForm'
-import LoginForm from './components/LoginForm'
+import RegistrationForm from './components/RegistrationForm'
+// import LoginForm from './components/LoginForm'
 // import OldYoutubeForm from './components/OldYoutubeForm'
 
 function App() {
   return (
     <div className="App">
       {/* <OldYoutubeForm /> */}
-      <LoginForm />
+      <RegistrationForm />
     </div>
   );
 }
