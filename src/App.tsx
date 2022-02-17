7f5ae933021db63f9e4a8785724d65413f689e7d
GabrielRioo <gabrielpaivario@gmail.com>
2022-02-16 23:58:06 -0300

    Criando um formilario de login com validação

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
-import FormikContainer from './components/FormikContainer'
-import NewYoutubeForm from './components/NewYoutubeForm'
+// import FormikContainer from './components/FormikContainer'
+// import NewYoutubeForm from './components/NewYoutubeForm'
+import LoginForm from './components/LoginForm'
 // import OldYoutubeForm from './components/OldYoutubeForm'
 
 function App() {
   return (
     <div className="App">
       {/* <OldYoutubeForm /> */}
-      <FormikContainer />
+      <LoginForm />
     </div>
   );
 }
